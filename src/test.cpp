7fa1bc8cc026b8cb64cd053ecd604d1b8d6ef132
Sean <Sean_Cummings@student.uml.edu>
2017-09-14 17:14:26 -0400

    changed program to get input

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,10 @@
-#include "stdio.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char const *argv[]) {
   int add1, add2, sum;
-  add1 = 1;
-  add2 = 2;
+  cin >> add1 >> add2;
   sum = add1 + add2;
-  printf("%d\n", sum );
+  cout << sum << endl;
   return 0;
 }
